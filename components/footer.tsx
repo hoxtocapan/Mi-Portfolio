b046c615beb9ef2572b2877ce2d7b23d52c15a27
hoxtocapan <Hoxtocapan@gmail.com>
2024-01-05 23:19:29 -0600

    actualizacion de certificados

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="mb-10 px-4 text-center text-gray-500">
       <small className="mb-2 block text-xs">
-        &copy; 2024 Hugo Oxtocapan Mora. Reservados todos los derechos.
+        &copy; 2024 Hugo Oxtocapan Mora. Todos los derechos reservados.
       </small>
     </footer>
   );
